f4ab9157071c4079a8c3e8f336795b6e9e458a5c
Sydney Andrade <syddevious@gmail.com>
2022-06-15 20:06:06 -0500

    fixed fib test

--- tests/test_worksheets/test_misc.py
+++ tests/test_worksheets/test_misc.py
@@ -2,13 +2,15 @@ import pytest
 import sys
 sys.path.append("../../")
 from worksheets.misc.fibonnaci import fibonnaci
+from worksheets_answers.misc.fibonnaci import fibonnaci as fibonnaci_a
 
 class TestMisc:
     def test_fibonacci(self):
-        assert fibonnaci(0) == 0
-        assert fibonnaci(1) == 1
-        assert fibonnaci(2) == 1
-        assert fibonnaci(10) == 55
-        assert fibonnaci(20) == 6765
-        assert fibonnaci(79) == 14472334024676221
+        assert fibonnaci(5) is not None
+        assert fibonnaci(0) == fibonnaci_a(0)
+        assert fibonnaci(1) == fibonnaci_a(1)
+        assert fibonnaci(2) == fibonnaci_a(2)
+        assert fibonnaci(10) == fibonnaci_a(10)
+        assert fibonnaci(20) == fibonnaci_a(20)
+        assert fibonnaci(79) == fibonnaci_a(79)
 
