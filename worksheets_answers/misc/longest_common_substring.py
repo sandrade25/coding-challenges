46a1d64eb52978a32d1197031bf78ef532ffd086
Sydney Andrade <syddevious@gmail.com>
2022-06-16 20:37:13 -0500

    worksheet answers complete for misc

--- worksheets_answers/misc/longest_common_substring.py
+++ worksheets_answers/misc/longest_common_substring.py
@@ -4,4 +4,15 @@
 
 
 def longest_common_substring(str1, str2):
-    pass
+    m = len(str1)
+    n = len(str2)
+    matrix = [[0 for i in range(n + 1)] for i in range(m + 1)]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if str1[i - 1] == str2[j - 1]:
+                matrix[i][j] = matrix[i-1][j-1] + 1
+            else:
+                matrix[i][j] = max(matrix[i-1][j], matrix[i][j-1])
+
+    return matrix[m][n]
