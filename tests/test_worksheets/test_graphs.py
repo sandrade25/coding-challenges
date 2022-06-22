425f81d21f16668954ee4a67628e09d5c23dff7b
Sydney Andrade <syddevious@gmail.com>
2022-06-21 20:40:38 -0500

    completed test for dijkstras shortest path

--- tests/test_worksheets/test_graphs.py
+++ tests/test_worksheets/test_graphs.py
@@ -6,9 +6,20 @@ from worksheets_answers.graphs.dijkstras_shortest_path import dijkstras_shortest
 from worksheets.graphs.dijkstras_shortest_path import dijkstras_shortest_path
 
 class TestGraphs:
-    def test_binary_search(self):
-        return
+    def test_dijkstras_shortest_path(self, weighted_graph):
+        
+        answer1 = dijkstras_shortest_path_a(weighted_graph, "s", "t")
+        answer2 = dijkstras_shortest_path_a(weighted_graph, "t", "b")
+        answer3 = dijkstras_shortest_path_a(weighted_graph, "d", "s")
 
+        user1 = dijkstras_shortest_path_a(weighted_graph, "s", "t")
+        assert user1 is not None, "solution not yet provided"
+        user2 = dijkstras_shortest_path_a(weighted_graph, "t", "b")
+        user3 = dijkstras_shortest_path_a(weighted_graph, "d", "s")
+        
+        assert user1 == answer1
+        assert user2 == answer2
+        assert user3 == answer3
     
 
 
