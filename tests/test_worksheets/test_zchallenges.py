fd64a19d67c2af9df58ea300d71f908fe660e478
Sydney Andrade <syddevious@gmail.com>
2022-06-22 20:34:06 -0500

    added additional test for dfs connected cell in grid. and have working answer.

--- tests/test_worksheets/test_zchallenges.py
+++ tests/test_worksheets/test_zchallenges.py
@@ -34,19 +34,27 @@ class TestZChallenges:
         assert answer1 == user1
         assert answer2 == user2
 
-    def test_dfs_connected_cell_in_grid(self, binary_grid1, binary_grid2, binary_grid3):
+    def test_dfs_connected_cell_in_grid(self, binary_grid1, binary_grid2, binary_grid3, binary_grid4):
         answer1 = dfs_connected_cell_in_grid_a(binary_grid1)
+        assert answer1 == 8
         answer2 = dfs_connected_cell_in_grid_a(binary_grid2)
+        assert answer2 == 5
         answer3 = dfs_connected_cell_in_grid_a(binary_grid3)
+        assert answer3 == 10
+        answer4 = dfs_connected_cell_in_grid_a(binary_grid4)
+        assert answer4 == 1
 
         user1 = dfs_connected_cell_in_grid(binary_grid1)
         assert user1 is not None, "solution not yet provided"
         user2 = dfs_connected_cell_in_grid(binary_grid2)
         user3 = dfs_connected_cell_in_grid(binary_grid3)
+        user4 = dfs_connected_cell_in_grid(binary_grid4)
 
         assert answer1 == user1
         assert answer2 == user2
         assert answer3 == user3
+        assert answer4 == user4
+
 
         
 
--- worksheets_answers/zchallenges/dfs_connected_cell_in_grid.py
+++ worksheets_answers/zchallenges/dfs_connected_cell_in_grid.py
@@ -11,18 +11,33 @@ Note that each cell in a region is connected to at least one other cell in the r
 Given an m x n matrix, find and print the number of cells in the largest region in the matrix.
 
 """
+
+
+"""
+
+[
+    [1, 0, 1, 1, 0],
+    [1, 1, 0, 0, 1], 
+    [0, 1, 1, 1, 0], 
+    [0, 0, 0, 0, 1], 
+    [1, 1, 1, 0, 0]
+]
+"""
+
+
+
 allowed_moves = [(-1,0), (-1,-1), (-1,1), (0,-1), (0, 1), (1, -1), (1, 0), (1,1)]
 
-def dfs(grid, m, n, v, rows, cols, cell_count):
+def dfs(grid, m, n, v, rows, cols):
 
     v.append((m, n))
     for move in allowed_moves:
         row = move[0] + m
         col = move[1] + n
         if row < rows and row >= 0 and col < cols and col >= 0 and grid[row][col] == 1 and (row, col) not in v:
-            cell_count += dfs(grid, row, col, v, rows, cols, cell_count)
+            dfs(grid, row, col, v, rows, cols)
 
-    return cell_count
+    return v
 
     
     
@@ -33,12 +48,14 @@ def dfs_connected_cell_in_grid(grid):
     rows = len(grid)
     cols = len(grid[0])
     largest_cell_count = 0
-    v = []
+    v = set()
 
     for i in range(rows):
         for j in range(cols):
             if (i, j) not in v and grid[i][j] == 1:
-                cell_count = dfs(grid, i, j, v, rows, cols, 1)
+                visited = dfs(grid, i, j, [], rows, cols)
+                cell_count = len(visited)
+                v = v.union(visited)
                 if cell_count > largest_cell_count:
                     largest_cell_count = cell_count
             
